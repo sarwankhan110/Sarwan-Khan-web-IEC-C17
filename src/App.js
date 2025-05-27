992b68a756f816f4e017c3aa75ff66902207d378
Sarwan Khna <sarwan.khan.mangi110@gmail.com>
2025-05-27 05:33:07 +0500

    added a paragraph

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       {/* ctrl+space to to get the refernace of component */}
       <h1>Ny First React App</h1>
       <button type="button" className="btn btn-primary px-5">Primary</button>
+      <p>Now i have made some changes in code and pushing again</p>
     </div>
   );
 }
