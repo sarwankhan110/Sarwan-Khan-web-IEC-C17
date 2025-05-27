48b54e3e81c0faae5cba5a364e97fecb25888877
Sarwan Khna <sarwan.khan.mangi110@gmail.com>
2025-05-27 06:05:42 +0500

    Made a few change and then will merge

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
       <h1>Ny First React App</h1>
       <button type="button" className="btn btn-primary px-5">Primary</button>
       <p>Now i have made some changes in code and pushing again</p>
+      <p>Now i have made some ther changes and going to merge in master</p>
+
     </div>
   );
 }
